37919296e9b4fbd8e8e020944a937a2e14f4063a
Korolovych Jacint <stronger@inf.elte.hu>
2020-12-15 18:26:41 +0100

    Basic authentication included

---
+++ blackjack/src/components/App.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Signup from "./auth/sign-up"
+import Login from "./auth/sign-in"
+import ForgotPassword from "./auth/password-reset.js"
+import UpdateProfile from "./update-profile"
+import PrivateRoute from "./PrivateRoute";
+
+function App(){
+  return (
+      <AuthProvider>
+          <Container className="d-flex align-items-center justify-content-center"
+                     style={{ minHeight: "100vh"}}
+          >
+              <div className="w-100" style={{maxWidth: "400px"}}>
+                  <Router>
+                      <AuthProvider>
+                          <Switch>
+                              <PrivateRoute exact path="/" component={Dashboard}/>
+                              <PrivateRoute path="/update-profile" component={UpdateProfile}/>
+                              <Route path="/signup" component={Signup}/>
+                              <Route path="/login" component={Login}/>
+                              <Route path="/forgot-password" component={ForgotPassword}/>
+                          </Switch>
+                      </AuthProvider>
+                  </Router>
+              </div>
+          </Container>
+      </AuthProvider>
+  )
+}
+
+export default App
--- blackjack/src/firebase.js
+++ blackjack/src/firebase.js
@@ -3,14 +3,17 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBsc8XJ3kk8dKozRhMXrIwS_XBKho3fKUc",
-    authDomain: "blackjack-c8ab6.firebaseapp.com",
-    projectId: "blackjack-c8ab6",
-    storageBucket: "blackjack-c8ab6.appspot.com",
-    messagingSenderId: "849105990844",
-    appId: "1:849105990844:web:84e36d89ff3ab377f28cb9"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-firebase.initializeApp(firebaseConfig);
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = app.auth();
+export default app
+
+
