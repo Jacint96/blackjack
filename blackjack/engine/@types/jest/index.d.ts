afbaf774388fd932452e9730b7550670c95bf6b7
Attila Tóth <muchcoffe@gmail.com>
2021-05-24 21:31:57 +0200

    custom matcher, jest setup

---
+++ blackjack/engine/@types/jest/index.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace jest {
+    interface Matchers<R, T = {}> extends jest.Matchers<R, T> {
+      eqWithMsg(received: any, target: any, message: string | null = null, strict = true): R;
+    }
+  }
+}
+
+
