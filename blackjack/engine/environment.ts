400d49746e4a32047c7e2054a2b8df7584392dc2
Tóth Attila <muchcoffe@gmail.com>
2021-05-26 18:17:46 +0200

    env example

--- blackjack/engine/environment.ts
+++ blackjack/engine/environment.ts
@@ -5,10 +5,12 @@ dotenv.config({ path: path.resolve(__dirname, '.env') })
 
 interface Environment {
   mongoDb: string
+  redisHost: string
 }
 
 const envConfig: Environment = {
   mongoDb: process.env.MONGODB_URI ?? 'mongodb://localhost:27017',
+  redisHost: process.env.REDIS_HOST ?? 'redis://localhost:6379',
 }
 
 export default envConfig
